3528fd9b07ba7bc646a0ddbc2e259788a8dcaa40
Wietze <wietze@users.noreply.github.com>
2024-02-27 00:45:50 +0000

    Updating OptionCharSubstitution modifier

--- Modifiers/OptionCharSubstitution.psm1
+++ Modifiers/OptionCharSubstitution.psm1
@@ -2,17 +2,15 @@ using module "..\Types\Modifier.psm1"
 using module "..\Types\Token.psm1"
 
 class OptionCharSubstitution : Modifier {
-    [char]$ProvidedOptionChar;
     [char[]]$OutputOptionChars;
 
-    OptionCharSubstitution([Token[]]$InputCommandTokens, [string[]]$ExcludedTypes, [bool]$Probability, [char]$ProvidedOptionChar, [char[]]$OutputOptionChars) : base($InputCommandTokens, $ExcludedTypes, $Probability) {
-        $this.ProvidedOptionChar = $ProvidedOptionChar;
+    OptionCharSubstitution([Token[]]$InputCommandTokens, [string[]]$ExcludedTypes, [float]$Probability, [object]$OutputOptionChars) : base($InputCommandTokens, $ExcludedTypes, $Probability) {
         $this.OutputOptionChars = $OutputOptionChars;
     }
 
     [void]GenerateOutput() {
         foreach ($Token in $this.InputCommandTokens) {
-            if (!$this.ExcludedTypes.Contains($Token.Type) -and $Token.TokenContent[0] -eq $this.ProvidedOptionChar) {
+            if (!$this.ExcludedTypes.Contains($Token.Type) -and ($this.OutputOptionChars | ForEach-Object {$_ -eq $Token.TokenContent[0] }) -contains $true) {
                 $Token.TokenContent[0] = [Modifier]::ChooseRandom($this.OutputOptionChars);
             }
         }
