3591fa263245235183d4742f5a5fb1588a589fae
Wietze <wietze@users.noreply.github.com>
2024-04-13 16:48:46 +0100

    Improving QuoteInsertion logic

--- Modifiers/QuoteInsertion.psm1
+++ Modifiers/QuoteInsertion.psm1
@@ -13,21 +13,33 @@ class QuoteInsertion : Modifier {
         foreach ($Token in $this.InputCommandTokens) {
             $NewTokenContent = [System.Collections.ArrayList]@();
             $i = 0;
+            $index = 0;
             if (!$this.ExcludedTypes.Contains($Token.Type)) {
+                $Content = $Token.ToString();
                 foreach ($Char in $Token.TokenContent) {
                     $NewTokenContent.Add($Char);
-                    if ( [Modifier]::CoinFlip($this.Probability) -and !( $NewTokenContent[-1] -eq [QuoteInsertion]::QuoteChar)) {
-                        #do {
+                    if ([Modifier]::CoinFlip($this.Probability) -and `
+                            ( ( $Content[-1] -ne [QuoteInsertion]::QuoteChar ) `
+                            -or ( ($index -gt 0) -and `
+                            ($Char -match '^[a-zA-Z0-9]$') -and `
+                            ($index -lt ($Content.Length - 2)) -and `
+                            ($Content[$index+1] -match '^[a-zA-Z0-9]$')) `
+                    )){
                         $NewTokenContent.Add([QuoteInsertion]::QuoteChar);
                         $i++;
-                        ##} while ([Modifier]::CoinFlip($this.Probability * [Math]::Pow(0.9, $i)));
                     }
+                    $index++;
                 }
 
                 # Check if there are unbalanced quotes
                 if (($i % 2) -ne 0) {
                     if ($NewTokenContent[-1] -eq [QuoteInsertion]::QuoteChar) {
-                        $NewTokenContent.RemoveAt($NewTokenContent.Count - 1);
+                        if ($content[-1] -eq [QuoteInsertion]::QuoteChar) {
+                            # Original token ended with quote, find the penultimate one
+                            $NewTokenContent.RemoveAt(($NewTokenContent[0..($NewTokenContent.Count - 2)] -join '').lastIndexOf([QuoteInsertion]::QuoteChar));
+                        } else {
+                            $NewTokenContent.RemoveAt($NewTokenContent.Count - 1);
+                        }
                     }
                     else {
                         $NewTokenContent.Add([QuoteInsertion]::QuoteChar);
